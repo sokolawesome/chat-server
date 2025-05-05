6e474ffb894614a794284d61ca2b1e2d54801bc5
sokolawesome <sokolawesome@gmail.com>
2025-05-05 03:57:56 +0300

    feat(auth): preparation for JWT auth
    - add env variable for jwt secret
    - add jwt to config

--- config/config.go
+++ config/config.go
@@ -8,16 +8,21 @@ import (
 type Config struct {
 	ServerPort  string
 	DatabaseUrl string
+	JwtSecret   string
 }
 
 func Load() (*Config, error) {
 	cfg := Config{
 		ServerPort:  getEnv("SERVER_PORT", "8080"),
 		DatabaseUrl: getEnv("DATABASE_URL", ""),
+		JwtSecret:   getEnv("JWT_SECRET", ""),
 	}
 
 	if cfg.DatabaseUrl == "" {
-		log.Fatal("error: DATABASE_URL environment variable is required")
+		log.Fatal("fatal: DATABASE_URL environment variable is required")
+	}
+	if cfg.JwtSecret == "" {
+		log.Fatal("fatal: JWT_SECRET environment variable is required")
 	}
 
 	return &cfg, nil
